fe6258c6a17f9eab004eaa40839e42635658478f
JCGithu <jack.c.gracie@gmail.com>
2022-08-21 17:53:09 +0100

    adding favicon

--- src/App.svelte
+++ src/App.svelte
@@ -44,6 +44,13 @@
   });
 </script>
 
+<svelte:head>
+  <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
+  <link rel="manifest" href="favicon/site.webmanifest" />
+</svelte:head>
+
 <main>
   <h1 style:-webkit-filter={clicked.on ? "blur(5px)" : "blur(0px)"}>Haus of Cards</h1>
   {#if clicked.on}
