7e48aa2d2784607c086bf4d7f0709c6638fa7da7
JCGithu <jack.c.gracie@gmail.com>
2022-10-27 23:47:26 +0100

    more testing!

--- src/Card.svelte
+++ src/Card.svelte
@@ -31,7 +31,7 @@
   let mousePercentX = 0;
   let mousePercentY = 0;
   let mouseSheen = 0;
-  let tilt = 0;
+  let wow = "test";
 
   function cardMove(e) {
     //console.log(e);
@@ -42,20 +42,22 @@
     let mouseY = cardCenterY - e.clientY;
     mousePercentX = (mouseX / (cardPosition.width / 2)) * 15;
     mousePercentY = (mouseY / (cardPosition.height / 2)) * -10;
-    console.log(mousePercentX, mousePercentY);
+    //console.log(mousePercentX, mousePercentY);
     mouseSheen = ((e.clientY - cardPosition.top) / (cardPosition.bottom - cardPosition.top)) * 100;
   }
 
   window.addEventListener("deviceorientation", (ev) => {
     // ev.alpha -- steering wheel, ev.beta -- tilt, ev.gamma -- spin 0 is front;
+    let wow = ev.toString();
     let spin = ev.gamma / 3;
     if (spin > 15) spin = 15;
     if (spin < -15) spin = -15;
     mousePercentX = spin * -1;
-    tilt = 50 - ev.beta;
+    let tilt = 50 - ev.beta;
     if (tilt > 30) tilt = 30;
     if (tilt < 30) tilt = 30;
     mousePercentY = tilt / 3;
+    mouseSheen = Math.abs(tilt) * 3.33;
   });
 
   function cardClick(e) {
@@ -77,7 +79,7 @@
 </script>
 
 <section out:fade class="cardContainer" style="background-color:{styling.background};">
-  <span>{tilt},{mousePercentY}</span>
+  <span>{wow},{mousePercentY}</span>
   <p id="closer" on:click={close}>X</p>
   <div on:click={cardClick} class="card" style:transform={`perspective(400px) rotateY(${mousePercentX + (flipped ? 180 : 0)}deg) rotateX(${mousePercentY}deg)`} style:--degree={`${(mousePercentX - mousePercentY) / 2}deg`} style:--p1="{Math.round(mouseSheen) - 10}%" style:--p2="{Math.round(mouseSheen)}%" style:--p3="{Math.round(mouseSheen) + 10}%" style:left={styling.left} style:top={styling.top} on:mousemove={cardMove} on:mouseleave|self={mouseLeave}>
     <img id="cardBack" style={`z-index:${styling.z}; width:${styling.width}; height:${styling.height}`} src="/images/Card_Back.png" alt={clicked.alt} />
